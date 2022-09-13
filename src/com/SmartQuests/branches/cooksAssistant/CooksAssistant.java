92457f138895328ef079eb4aadc6635769ab0c52
isaac-rosterlab <isaac@rosterlab.com>
2022-09-13 21:06:20 +1200

    ADD: cooks assistant instructions

---
+++ src/com/SmartQuests/branches/cooksAssistant/CooksAssistant.java
@@ -0,0 +1,18 @@
+package com.SmartQuests.branches.cooksAssistant;
+
+public class CooksAssistant {
+    //talk to new SmartObject(new Area.Rectangular(new Coordinate(3206, 3217, 0), new Coordinate(3210, 3212, 0)), "Cook", "Npc"))
+    // Say: Actually, I know where to find this stuff
+
+    //Pick up SmartObject(new Area.Rectangular(new Coordinate(3206, 3217, 0), new Coordinate(3210, 3212, 0)), "Pot", "GameObject"))
+
+    //A bucket spawns in the cellar of Lumbridge Castle,
+
+    //Go east out of Lumbridge Castle and run north along the path until you reach the chicken coop north-west of Farmer Fred's house. Go into the coop and pick up an egg.
+
+    //With an empty bucket, go north into the cow pasture. Use the empty bucket on a dairy cow to get a bucket of milk. Optional: You may talk to Gillie Groats across the river to the east in Lumbridge cow field next to the dairy cow for an explanation on how to get milk from it.
+
+    //With an empty pot, leave the cow pasture and go south-west to the wheat field. Pick up a grain then enter the Mill Lane Mill to the south-west. Optional: You may talk to Millie Miller inside the mill for an explanation on how to make flour. Climb to the top floor, put the grain in the hopper, pull the lever, then climb back down to the 1st floor[US]. Use an empty pot on the flour bin full of flour to get a pot of flour.
+
+    //talk to new SmartObject(new Area.Rectangular(new Coordinate(3206, 3217, 0), new Coordinate(3210, 3212, 0)), "Cook", "Npc"))
+}
